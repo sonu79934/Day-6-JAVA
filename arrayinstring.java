5521d65629d94f59c2968082a0e592d6904828ae
sonu79934 <yogasritha8@gmail.com>
2024-12-07 16:04:15 +0530

    Add files via upload

---
+++ arrayinstring.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class arrayinstring{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        String arr[]=new String[a];
+        for(int i=0;i<a;i++)
+        {
+           arr[i]=sc.next();
+        }
+        for(int i=0;i<a;i++)
+        {
+            System.out.println(arr[i]+" ");
+        }
+    }
+}
