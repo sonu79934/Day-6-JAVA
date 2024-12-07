f73cf7b72326442ef5f56f8595d7fb92f1c2d8a6
sonu79934 <yogasritha8@gmail.com>
2024-12-07 13:42:12 +0530

    Add files via upload

---
+++ sumofarrayelements.java
@@ -0,0 +1,15 @@
+import java.util.*;
+ public class sumofarrayelements{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        int s=0;
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+            s+=arr[i];
+        }
+        System.out.println(s);
+    }
+ }
